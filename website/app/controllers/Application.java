82378a030eefe53c3da920065a2c0772959fea07
Elias Haroun <elias@bamstrategy.com>
2015-08-11 13:57:32 -0400

    testing the front-end

--- website/app/controllers/Application.java
+++ website/app/controllers/Application.java
@@ -1,15 +1,18 @@
 package controllers;
 
-import play.*;
 import play.mvc.*;
-
+import play.data.*;
+import static play.data.Form.*;
 import views.html.*;
+import models.*;
+import java.util.*;
 
 public class Application extends Controller {
 
-    public Result index()
-    {
-        return ok(index.render("My first play app"));
+    /**
+     * Handle default path requests
+     */
+    public Result index() {
+        return ok(index.render("Hi"));
     }
-
 }
