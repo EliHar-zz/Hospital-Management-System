ae58da73b42c8abc8f4385c8fd6dcbf8e3c3e662
Richard Grand'Maison <rich.grandmaison@gmail.com>
2015-08-19 21:57:47 -0400

    finished techs

--- new-website/php/fetch-tech-salary.php
+++ new-website/php/fetch-tech-salary.php
@@ -10,20 +10,21 @@ $start_date_string = $_SESSION['start_date'];
 $start_date_date = date('Y/m/d', strtotime($start_date_string));
 $years_of_service = floor((date('Y/m/d') - $start_date_date));
 
-$hourly_rate = $_SESSION['base_hourly_rate'] + floor($years_of_service / $_SESSION['years_to_raise']) * $_SESSION['raise_amount'];
+$sql = "SELECT base_hourly_rate, raise_amount, years_to_raise, hours_per_week FROM technicians WHERE employee_types_id = '".$_SESSION['employee_type_id']."'";
+$result = $conn->query($sql);
+print_r($row);
+$row = mysqli_fetch_assoc($result);
 
-$hours = 0;
-$otHours = 0;
-
-while($row = mysqli_fetch_assoc($result)){
-	$hours += 12;
-	$endTime = strtotime($row['end_time']);
-	$startTime = strtotime($row['start_time']);
-	$otHours += ($endTime - $startTime)/3600 - 12;
-	}
+$hourly_rate = $row['base_hourly_rate'] + floor($years_of_service / $row['years_to_raise']) * $row['raise_amount'];
 	
-$salary = $hours * $hourly_rate + $otHours * $hourly_rate * $_SESSION['overtime_rate'];
-echo $salary;
+$salary = $row['hours_per_week'] * $hourly_rate;
+$start_String = strtotime($start);
+$end_String = strtotime($end);
+$days = ($end_String - $start_String)/86400;
+
+$proratedSalary = $salary * $days / 365;
+
+echo number_format((float)$proratedSalary, 2, '.', '');
 
 $conn->close();
 ?>
